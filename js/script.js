5d77d75e3487ddf5007530971dfa235e9b94b780
josephlbaker <joelucasbaker@gmail.com>
2019-02-08 17:31:00 -0800

    added modal functionality to all images -- not DRY

--- js/script.js
+++ js/script.js
@@ -32,22 +32,46 @@ function showSlides(n) {
 
 //LIGHTBOX
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal1 = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
+var img1 = document.getElementById('myImg1');
+var img2 = document.getElementById('myImg2');
+var img3 = document.getElementById('myImg3');
+var modalImg1 = document.getElementById("img01");
+var modalImg2 = document.getElementById("img02");
+var modalImg3 = document.getElementById("img03");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
+img1.onclick = function(){
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img2.onclick = function(){
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementById("myModal");
+var span1 = document.getElementById("myModal1");
+var span2 = document.getElementById("myModal2");
+var span3 = document.getElementById("myModal3");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
 }
