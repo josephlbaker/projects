39819bcb2d5ae8d1c97ba450d8d550370ca427a8
josephlbaker <joelucasbaker@gmail.com>
2019-02-08 16:45:41 -0800

    added lightbox functionality to first image only

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+//CAROUSEL
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +28,26 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+//LIGHTBOX
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("myModal");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
